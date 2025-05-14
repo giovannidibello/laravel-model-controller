b117a803c7c172aa718d7d1fd0f4b86704c8d2b4
Giovanni <dibellogio@gmail.com>
2025-05-14 10:57:08 +0200

    add controller

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $movies = Movie::all();
+        // dd($movies);
+
+        return view('home', compact("movies"));
     }
 
 
