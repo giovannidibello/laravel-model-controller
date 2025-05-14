ea0be54b7db0cb8e1e52f28b2063291affcd1a70
Giovanni <dibellogio@gmail.com>
2025-05-14 10:57:37 +0200

    add homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,5 +5,29 @@
 @endsection
 
 @section('contenuto')
-    <h1>Contenuto Pagina</h1>
+
+<div class="container">
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3">
+
+        @foreach ($movies as $movie)
+        <div class="col">
+            <x-card>
+
+                <x-slot:title>{{$movie["title"]}}</x-slot>
+            
+                <x-slot:original_title>{{$movie["original_title"]}}</x-slot>
+                
+                <x-slot:nationality>{{$movie["nationality"]}}</x-slot>
+
+                <x-slot:date>{{$movie["date"]}}</x-slot>
+
+                <x-slot:vote>{{$movie["vote"]}}</x-slot>
+            
+                </x-card>
+        </div>      
+        @endforeach     
+        
+    </div>
+</div>
+     
 @endsection
